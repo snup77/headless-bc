ebfc195fce72791aa044bccc1fc831a442612bb4
Anup Sheth <anup.sheth@gmail.com>
2022-04-23 22:02:32 -0400

    add loading spinner to product page; refactoring

--- pages/[product].js
+++ pages/[product].js
@@ -1,10 +1,20 @@
+import { useState } from "react"
 import Image from "next/image"
 import { getProductSlugs, getProduct, createCheckout } from "../helpers/shopify"
 
 function ProductDetails({ productData }) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const imageNode = productData.images.edges[0].node
+  const title = productData.title
+  const price = productData.priceRange.maxVariantPrice.amount.replace(/\.0/g, '')
+  const description = productData.description
+  const productVariant = productData.variants.edges[0].node.id
+
   
   async function checkout() {
-    const { webUrl } = await createCheckout(productData.variants.edges[0].node.id)
+    setIsLoading(true)
+    const { webUrl } = await createCheckout(productVariant)
     window.location.href = webUrl
 
   }
@@ -22,7 +32,7 @@ function ProductDetails({ productData }) {
       <div className="w-full flex flex-1 bg-gray-100">
         <div className="h-96 py-16 p10 flex flex-1 justify-center items-center w-full h-full relative">
           <Image
-            src={productData.images.edges[0].node.url}
+            src={imageNode.url}
             alt="Inventory item"
             className="object-scale-down max-h-full"
             layout='fill'
@@ -36,16 +46,22 @@ function ProductDetails({ productData }) {
            sm:mt-0 mt-2 text-5xl font-light leading-large
           "
         >
-          {productData.title}
+          {title}
         </h1>
-        <h2 className="text-2xl tracking-wide sm:py-8 py-6">${productData.priceRange.maxVariantPrice.amount.replace(/\.0/g, '')}</h2>
-        <p className="text-gray-600 leading-7">{productData.description}</p>
+        <h2 className="text-2xl tracking-wide sm:py-8 py-6">${price}</h2>
+        <p className="text-gray-600 leading-7">{description}</p>
         <div className="my-6"></div>
         <button
-          className="text-sm font-bold tracking-wider bg-transparent hover:bg-black text-black font-semibold hover:text-white py-4 px-12 border-2 border-black hover:border-transparent w-full"
+          className="text-sm font-bold tracking-wider bg-black text-white font-semibold py-4 px-12 border-2 border-black hover:border-transparent w-full"
           onClick={checkout}
         >
-          <div>Buy</div>
+          {isLoading && (
+            <svg class="inline animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          )}
+          <span>Buy</span>
         </button>
       </div>
     </div>
